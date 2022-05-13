25daa953d422f2cdf881d8f471521001be462588
ictus <ictus@ictusui-MacBookPro.local>
2022-05-13 16:52:39 +0900

    Updates

---
+++ precache-manifest.a3b1756e981d97eb44a86e2915345484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b64b60f8647b996377d9c5e269f7652",
+    "url": "/react-masterclass-challenge/index.html"
+  },
+  {
+    "revision": "0c8c8dbc9f7ff43e2bb4",
+    "url": "/react-masterclass-challenge/static/js/2.34afb7e4.chunk.js"
+  },
+  {
+    "revision": "3fd306d1532735ae8af93e8e05d2e68b",
+    "url": "/react-masterclass-challenge/static/js/2.34afb7e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc31c7d1a4a37372f48",
+    "url": "/react-masterclass-challenge/static/js/main.b171eed4.chunk.js"
+  },
+  {
+    "revision": "c59c856372524afba863",
+    "url": "/react-masterclass-challenge/static/js/runtime-main.12602559.js"
+  }
+]);
